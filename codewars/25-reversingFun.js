9526d033a18009c5757adbe428823ee5df015a55
princeofverry <verrykurniawan@students.undip.ac.id>
2024-02-26 07:58:14 +0700

    day 25

---
+++ codewars/25-reversingFun.js
@@ -0,0 +1,21 @@
+/*
+Let's say you start with this: "012345"
+
+The first thing you do is reverse it:"543210"
+Then you will take the string from the 1st position and reverse it again:"501234"
+Then you will take the string from the 2nd position and reverse it again:"504321"
+Then you will take the string from the 3rd position and reverse it again:"504123"
+
+Continue this pattern until you have done every single position, and then you will return the string you have created.
+ For this particular number, you would return:"504132"
+*/
+
+function flipNumber(str) {
+  let result = str.split(""); // Convert the string to an array of characters
+  for (let i = 0; i < str.length; i++) {
+    let substring = result.slice(i); // Extract substring starting from index i
+    substring.reverse(); // Reverse the substring
+    result = result.slice(0, i).concat(substring); // Concatenate the reversed substring with the unchanged prefix
+  }
+  return result.join(""); // Convert the array back to a string and return
+}
