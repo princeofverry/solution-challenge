79b2967c1771412ba2894a32d47993d57271d17a
princeofverry <verrykurniawan@students.undip.ac.id>
2024-06-11 07:19:30 +0700

    tambah penjelasan increase triplet

--- Leetcode/Leetcode-75 Interview/334.IncreasingTripletSubsequence.py
+++ Leetcode/Leetcode-75 Interview/334.IncreasingTripletSubsequence.py
@@ -6,4 +6,18 @@ class Solution(object):
         """
         #  such that i < j < k and nums[i] < nums[j] < nums[k]
 
+        # ngebuat 2 angka pertama itu terkecil
         num_i = num_j = float('inf')
+        # num i = num j = tak hingga +
+
+        for num in nums:
+            # maksa cari i paling kecil
+            if num <= num_i:
+                num_i = num
+            # kalo dah dapet i terkecil cari terkecil setelah j
+            elif num <= num_j:
+                num_j = num
+            else:
+                return True
+
+        return False
